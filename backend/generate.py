de67f78bdf59a7456dafae5a9b795f9f09f8e9ce
dimple18 <135057748+dimp01@users.noreply.github.com>
2025-09-22 18:56:34 +0530

    Update generate.py

--- backend/generate.py
+++ backend/generate.py
@@ -1,10 +1,18 @@
-from backend.model.text import load_shap_e_pipeline
+
+from shap_e.models.download import load_model, load_config
+from shap_e.diffusion.gaussian_diffusion import diffusion_from_config
+
 from backend.model.loader import gen_image, load_diffusion_pipeline
+from backend.model.text import TextModel
 from backend.model.diffuser import DiffusionModel
 from backend.config import device
 
+# Load Shap-E models globally to save time
+image_model = load_model("image300M", device=device)
+text_model = load_model('text300M', device=device)
+xm = load_model("transmitter", device=device)
+diffusion = diffusion_from_config(load_config("diffusion"))
 
-text_pipe = load_shap_e_pipeline(device)
 diffusion_p = load_diffusion_pipeline(device)
 
 class GenerateModel:
@@ -25,7 +33,8 @@ class GenerateModel:
         self.return_dict = return_dict
 
     def text(self):
-        decoder_output = text_pipe(
+        text = TextModel()
+        latents = text.generate(
             self.prompt,
             guidance_scale=self.guidance_scale,
             num_inference_steps=self.steps,
@@ -34,7 +43,7 @@ class GenerateModel:
             return_dict=self.return_dict,
         )
 
-        return decoder_output
+        return lantents
 
     def diffusion(self):
         image = gen_image(self.prompt, diffusion_p)
