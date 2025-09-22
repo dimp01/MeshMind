91c623a2145261edf22b7b9ace094f5b3e6fa0c4
dimple18 <135057748+dimp01@users.noreply.github.com>
2025-09-22 19:08:48 +0530

    Update text.py

--- backend/model/text.py
+++ backend/model/text.py
@@ -1,18 +1,65 @@
-import torch
-import streamlit as st
-from diffusers import ShapEPipeline
+from shap_e.diffusion.sample import sample_latents
+from shap_e.util.notebooks import create_pan_cameras, decode_latent_images, gif_widget
 from backend.config import device
 
-@st.cache_resource
-def load_shap_e_pipeline(device=device):
-    """
-    Loads and caches the Shap-E pipeline for efficiency.
-    Returns: ShapEPipeline object
-    """
-    pipe = ShapEPipeline.from_pretrained(
-        "openai/shap-e",
-        torch_dtype=torch.float16
-    ).to(device)
-    return pipe
+class TextModel:
+    def __init__(self, model, diffusion, xm):
+        self.model = model
+        self.diffusion = diffusion
+        self.xm = xm
 
+        # Default generation parameters
+        self.guidance_scale = 15.0
+        self.use_karras = True
+        self.karras_steps = 64
+        self.sigma_min = 1e-3
+        self.sigma_max = 160
+        self.s_churn = 0
+        self.clip_denoised = True
+        self.use_fp16 = True
+        self.progress = True
+
+    def generate(
+        self,
+        prompt
+        guidance_scale=None,
+        karras_steps=None,
+        sigma_min=None,
+        sigma_max=None,
+        s_churn=None,
+        use_karras=None,
+        clip_denoised=None,
+        use_fp16=None,
+        progress=None,
+    ):
+        # Update parameters if provided
+        guidance_scale = guidance_scale or self.guidance_scale
+        karras_steps = karras_steps or self.karras_steps
+        sigma_min = sigma_min or self.sigma_min
+        sigma_max = sigma_max or self.sigma_max
+        s_churn = s_churn or self.s_churn
+        use_karras = use_karras if use_karras is not None else self.use_karras
+        clip_denoised = clip_denoised if clip_denoised is not None else self.clip_denoised
+        use_fp16 = use_fp16 if use_fp16 is not None else self.use_fp16
+        progress = progress if progress is not None else self.progress
+
+        # Generate latent 3D representation
+        latents = sample_latents(
+            batch_size=1,
+            model=self.model,
+            diffusion=self.diffusion,
+            guidance_scale=guidance_scale,
+            model_kwargs=dict(texts=[prompt] * batch_size),
+            progress=progresss,
+            clip_denoised=clip_denoised,
+            use_fp16=use_fp16,
+            use_karras=use_karras,
+            karras_steps=karras_steps,
+            sigma_min=sigma_min,
+            sigma_max=sigma_max,
+            s_churn=s_churn,
+        )
+
+        mesh = decode_latent_mesh(self.xm, latents[0])
+        return mesh
 
